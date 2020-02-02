3e8168df692685636f89a64b97c4c48ab4a9ce13
Vishwak Senan <vishwak1998@gmail.com>
2020-02-02 19:43:26 +0530

    class def for models

--- eg_project/nn_model/nn_model.py
+++ eg_project/nn_model/nn_model.py
@@ -1,4 +1,35 @@
 class EssayGraderClass:
     def grammer_and_spell_check(self):
-        
-        return True
+        """
+        Checks for grammer and spell check in the paragraphs and returns the evaluation mark
+        """
+        score = 5
+        return score
+    
+    def style_continuity_check(self):
+        """
+        Checks for style check in essay and returns the returns evaluation mark
+        """
+        score = 5
+        return score
+    
+    def lexical_resource_check(self):
+        """
+        Checks for high level lexical resource check and returns evaluation mark
+        """
+        score = 5
+        return score
+
+    def sentence_complexity_check(self):
+        """
+        Checks for complex sentence in paragraph and returns evaluation mark
+        """
+        score = 5
+        return score
+    
+    def coherence_cohesion_check(self):
+        """
+        Checks for idea connection between sentences and returns evaluation mark
+        """
+        score = 5
+        return score
--- eg_project/nn_model/views.py
+++ eg_project/nn_model/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+import nn_model
 # Create your views here.
+def evaluate():
+	"""
+	Evalutes the given paragraph and assigns scores in seperate variables
+	"""
+	# Calculating and getting the scores
+	core_engine = nn_model.EssayGraderClass()
+	grammer_score = core_engine.grammer_and_spell_check()
+	style_score = core_engine.style_continuity_check()
+	cc_score = core_engine.coherence_cohesion_check()
+	lexical_score = core_engine.lexical_resource_check()
+	complexity_score = core_engine.sentence_complexity_check()
+	
+	# Save all the scores as dictionary
+	score_context = {} 
+	score_context['grammer'] = grammer_score
+	score_context['style'] = style_score
+	score_context['cc'] = cc_score
+	score_context['lexical'] = lexical_score
+	score_context['complexity'] = complexity_score
